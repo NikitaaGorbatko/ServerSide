e18c954ffbfefafab8ab7bd1259370a71f77facd
nikita <nikitagorbatko072@gmail.com>
2019-05-13 15:53:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Runnable threadServerRunnable = new ServerRunnable();
+        Thread serverThread = new Thread(threadServerRunnable);
+        serverThread.start();
+        //https://docs.oracle.com/en/java/javase/12/docs/api/jdk.httpserver/com/sun/net/httpserver/package-summary.html
+        //http server
+    }
+}
